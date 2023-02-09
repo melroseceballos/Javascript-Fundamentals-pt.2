404f32b2ca7e161806b1553c2a7e51b6549fd756
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-08 22:05:22 -0800

    Exercise 4 done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ Exercise 2:
 */
 
 // Complete Exercise 2 below...
-
+foods.push("pizza" , "cheeseburger");
 
 
 console.log('Exercise 2 Result:\n', foods);
@@ -26,8 +26,7 @@ Exercise 3:
 */
 
 // Complete Exercise 3 below...
-
-
+foods.unshift("taco");
 
 console.log('Exercise 3 Result:\n', foods);
 
@@ -38,8 +37,7 @@ Exercise 4:
 
 // Complete Exercise 4 below...
 
-
-
+let favFood = foods [1];
 console.log('Exercise 4 Result:\n', favFood);
 
 /*
